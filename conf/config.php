3e0694db73acf1dbf6ceb0a757a42e2dfeb4dae6
titoko <titoko@titoko.fr>
2014-01-30 16:50:55 +0100

    no curl

---
+++ conf/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *  * @name        Config
+ *   * @author      Philipp Maurer
+ *    * @author      Tobias Reich
+ *     * @copyright   2014 by Philipp Maurer, Tobias Reich
+ *     */
+
+if(!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
+
+// Database configurations
+$dbHost = 'localhost'; //Host of the Database
+$dbUser = 'NAMETOCHANGE'; //Username of the database
+$dbPassword = 'PWDTOCHANGE'; //Password of the Database
+$dbName = 'NAMETOCHANGE'; //Database name
+
+?
