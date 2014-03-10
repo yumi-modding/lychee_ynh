85eb6a0a275b348c4e29598bac189f236fbbd5ec
titoko titoko <titoko@titoko.fr>
2014-03-10 20:20:21 +0100

    fix sed

--- conf/postini.sql
+++ conf/postini.sql
@@ -1,1 +1,1 @@
-UPDATE lychee_settings SET value ='#user'  WHERE \`key\` = 'username';UPDATE lyche_settings SET value ='#pwd' WHERE \`key\` = 'password';
+UPDATE lychee_settings SET value ='user'  WHERE `key` = 'username';UPDATE lychee_settings SET value ='pwd' WHERE `key` = 'password';
