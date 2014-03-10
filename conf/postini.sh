3bcf6a70f2fd4d1522e8b0e0892b58d1935edb29
titoko titoko <titoko@titoko.fr>
2014-03-10 19:13:53 +0100

    test multi

---
+++ conf/postini.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo echo "UPDATE lychee_settings SET value ='#user'  WHERE \`key\` = 'username';UPDATE lyche_settings SET value ='#pwd' WHERE \`key\` = 'password';" | mysql -u $db_user -p$db_pwd -D $db_user
